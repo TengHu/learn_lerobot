8eb7e6f43bc60873b5990fe391d9074ad687f56a
Teng Hu <hu.niel92@gmail.com>
2025-07-13 22:24:42 -0400

    .

--- lerobot-runpod-desktop/src/common/startup_scripts/user_startup.sh
+++ lerobot-runpod-desktop/src/common/startup_scripts/user_startup.sh
@@ -46,30 +46,10 @@ start_jupyter() {
     echo "Jupyter Lab started on port 8888."
 }
 
-install_conda_and_lerobot() {
-    export DISPLAY=:1
-
-    # Install conda
-    mkdir -p "~/miniconda3"
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "~/miniconda3/miniconda.sh"
-    bash "~/miniconda3/miniconda.sh" -b -u -p "~/miniconda3"
-    rm "~/miniconda3/miniconda.sh"
-
-    # Initialize conda
-    source "~/miniconda3/etc/profile.d/conda.sh"
-    source "~/miniconda3/bin/activate"
-    conda init --all
-
-    # Install aerobat (lerobot)
-    mkdir -p "$HOME/Workspace"
-    cd "$HOME/Workspace"
-    git clone https://github.com/huggingface/lerobot.git
-}
 
 # Main
 start_ssh
 # start_jupyter
-install_conda_and_lerobot
 
 # Keep script running
 sleep infinity 
